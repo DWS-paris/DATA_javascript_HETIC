f53898925b162218f4e64105c1d5ac8252dacfa6
DWS-paris <j.noyer@digitalworkshop.fr>
2019-01-08 14:58:10 +0100

    Fin de l'exercice image classifier

--- ml5js_image_classification/js/app.js
+++ ml5js_image_classification/js/app.js
@@ -1,60 +1,46 @@
-/* 
-Initialiser ML5JS
-- Utiliser le modèle MobileNet
-- Ajouter une fonction  dee callback
-*/
-    const classifier = ml5.imageClassifier( 'MobileNet', () => {
-        console.log('Le modèle est prêt !');
-    });
-//
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+const classifier = ml5.imageClassifier('MobileNet', () => {
+    console.log('Model is ready!')
+});
 
 /* 
 Variables
 */
-    // Variables globales
     let myImage = new Image();
-    let myResult = document.querySelector('#myResult');
-    let myList = document.querySelectorAll('li img');
-    
-    // Canvas
-    let myCanvas = document.querySelector('#myCanvas');
+    let myCanvas = document.createElement('canvas');
     let context = myCanvas.getContext('2d');
+    let myResult = document.querySelector('#myResult');
+    let resultList = document.createElement('ul');
 //
 
 /* 
-Fonction pour ajouter une image dans le canvas
+Function to create image canvas
 */
-    const createImageCanvas = imagePath => {
-        // Définir l'image
-        myImage.src = imagePath;
-
+    const createImageCanvas = (imgPath) => {
+        // Set image
+        myImage.src = imgPath;
+        
+        // Add image to canvas when loaded
         myImage.onload = () => {
+            // Set canvas size
             myCanvas.width = myImage.width;
             myCanvas.height = myImage.height;
+
+            // Draw image
             context.drawImage(myImage, 0, 0);
-        }
+            
+            // Get image classification
+            classifyImage(myImage);
+        };
     };
 //
 
-/* 
-Fonctions d'analyse
-*/
-    // Classification de l'image
-    const classifyImage = img => {
-        // Utilisation du modèle MobileNet
-        classifier.predict( img, 5, getPrediction )
-    };
-
-    // Prédictioon
-    const getPrediction = (err, results) => {
-        results ? getPredictionResults(results) : console.error(err);
-    }
-//
 
 /* 
-Afficher les résultats dans le DOM
+Function yo display prediction results
 */
-    const getPredictionResults = (results) => {
+    const getPredictionResult = (results) => {
+        console.log(results)
         // Append canvas
         document.querySelector('#myCanvas').appendChild(myCanvas);
 
@@ -68,14 +54,28 @@ Afficher les résultats dans le DOM
     }
 //
 
+/* 
+Functions for prediction
+*/
+    // Use ML5js to classify image
+    const classifyImage = (img) => {
+        // Specify the amount of classes you want in the result (3 by default)
+        classifier.predict(img, 5, getPrediction);
+    }
 
-
+    // Set classification callback function
+    const getPrediction = (err, results) => {
+        results ? getPredictionResult(results) : console.error(err);
+    };
+//
 
 
 /* 
-Start application 
+Start application
 */
-    document.addEventListener('DOMContentLoaded', () => {
-
-    });
+    document.querySelectorAll('ul img').forEach( item => {
+        item.addEventListener('click', () => {
+            createImageCanvas(item.getAttribute('src'));
+        })
+    } )
 //
